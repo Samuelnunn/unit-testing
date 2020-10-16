97aee842b03a0dddc919cca5cec0f794f931aa63
Kimi Zou <kimizou.kz@gmail.com>
2020-10-16 17:25:10 -0400

    Save submitted cat info. part 2

--- test/get-body-from-request-spec.js
+++ test/get-body-from-request-spec.js
@@ -1,6 +1,6 @@
-const EventEmitter = require('events');
-const { expect } = require('chai');
-const { getBodyFromRequest } = require('../get-body-from-request');
+const EventEmitter = require("events");
+const { expect } = require("chai");
+const { getBodyFromRequest } = require("../get-body-from-request");
 describe("The getBodyFromRequest function", () => {
   let fakeReq = null;
 
@@ -8,25 +8,41 @@ describe("The getBodyFromRequest function", () => {
     fakeReq = new EventEmitter();
   });
 
-  it('returns an empty string for no body', done => {
+  it("returns an empty string for no body", (done) => {
     //arrange
-   const response = getBodyFromRequest(fakeReq);
+    const response = getBodyFromRequest(fakeReq);
 
-    
     //act
-    fakeReq.emit('end');
-    
+    fakeReq.emit("end");
+
     //assert
     response.then((data) => {
-      if( data === "") {
-       done("");
+      if (data === "") {
+        done("");
       } else {
-        done("error!")
+        done("error!");
       }
     });
   });
 
-  it('returns the data read from the stream', done => {
-    expect.fail('please write this test');
+  it("returns the data read from the stream", (done) => {
+    //arrange
+    const promiseResponse = getBodyFromRequest(fakeReq);
+    const string1 = "doggy1";
+    const string2 = "doggy2";
+
+    //act
+    fakeReq.emit("data", string1);
+    fakeReq.emit("data", string2);
+    fakeReq.emit("end");
+
+    //assert
+    promiseResponse.then((data) => {
+      if (data === string1 + string2) {
+        done();
+      } else {
+        done("error!");
+      }
+    });
   });
 });
